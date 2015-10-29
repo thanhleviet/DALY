639413da2bd85566503638366baa31765a5c8941
Viet Thanh Le <thanhlv@Viets-MacBook-Pro.local>
2015-10-29 15:15:49 +0700

    Fix sensitivity outcome return as vector

--- R/sensitivity.R
+++ R/sensitivity.R
@@ -2,74 +2,55 @@
 
 sensitivity <-
 function(x, method = c("src", "pcc"), rank = FALSE, mapped = TRUE){
-  ## evaluate inputs
-  method <- match.arg(method)
-  if (!is.logical(rank) && !is.na(rank))
-    stop("'rank' must be a logical value", call. = FALSE)
-  if (!is.logical(rank) && !is.na(rank))
-    stop("'rank' must be a logical value", call. = FALSE)
-
-  ## aggregate DALYs
-  y <- aggregate(x, by = "total")
-  daly <- y$DALY
-
-  ## max number of parameters
-  ## n_outcomes * 8
-  n_outcomes <- length(x) - 2
-
-  ## merge unique columns
-  listNames <- c("inc", "trt", "ons", "dur", "DWt", "DWn", "mrt", "dth")
-  data <- numeric(length(daly))
-  for (i in seq(n_outcomes)){
-    for (j in seq(8)){
-      data <-
-        cbind(data,
-              as_column(x[[i]]$input[[j]], i, listNames[j]))
+    method <- match.arg(method)
+    if (!is.logical(rank) && !is.na(rank)) 
+        stop("'rank' must be a logical value", call. = FALSE)
+    if (!is.logical(rank) && !is.na(rank)) 
+        stop("'rank' must be a logical value", call. = FALSE)
+    y <- aggregate(x, by = "total")
+    daly <- y$DALY
+    n_outcomes <- length(x) - 2
+    listNames <- c("inc", "trt", "ons", "dur", "DWt", "DWn", 
+        "mrt", "dth")
+    data <- numeric(length(daly))
+    for (i in seq(n_outcomes)) {
+        for (j in seq(8)) {
+            data <- cbind(data, as_column(x[[i]]$input[[j]], 
+                i, listNames[j]))
+        }
     }
-  }
-
-  ## remove fixed values
-  fixed <- apply(data, 2, var) == 0
-  data <- data[, !fixed]
-
-  ## convert values to ranks
-  if (rank){
-    daly <- rank(daly)
-    data <- apply(data, 2, rank)
-  }
-
-  if (method == "src"){
-    ## scale variables
-    data <- as.data.frame(apply(data, 2, scale))
-    if (!mapped) daly <- scale(daly)
-
-    ## linear regression
-    out <- summary(lm(daly ~ ., data = data))
-
-  } else {
-    ## partial correlation coefficients
-    data <- as.matrix(data)
-    out <- matrix(ncol = 2, nrow = ncol(data))
-    colnames(out) <- c("rho", "p")
-    rownames(out) <- colnames(data)
-
-    for (i in seq(ncol(data))){
-      lm_y <- lm(daly ~ data[, -i])      # regress y to other x's
-      lm_x <- lm(data[, i] ~ data[, -i]) # regress x to other x's
-      out[i, ] <-
-        unlist(cor.test(lm_y$residuals, lm_x$residuals)[4:3],
-               use.names = FALSE)
+    fixed <- apply(data, 2, var) == 0
+    fixed.names <- which(fixed != TRUE)
+    # Using subset instead of eliminating the column index
+    # to have a resulted data frame
+    data <- subset(data, select = fixed.names)
+    #data <- data[, !fixed]
+    if (rank) {
+        daly <- rank(daly)
+        data <- apply(data, 2, rank)
     }
-  }
-
-  ## define S3 class
-  out_list <-
-    list(method = list(method = method, rank = rank, mapped = mapped),
-         out = out)
-  class(out_list) <- "DALY_sensitivity"
-
-  ## return output
-  return(out_list)
+    if (method == "src") {
+        data <- as.data.frame(apply(data, 2, scale))
+        if (!mapped) 
+            daly <- scale(daly)
+        out <- summary(lm(daly ~ ., data = data))
+    }
+    else {
+        data <- as.matrix(data)
+        out <- matrix(ncol = 2, nrow = ncol(data))
+        colnames(out) <- c("rho", "p")
+        rownames(out) <- colnames(data)
+        for (i in seq(ncol(data))) {
+            lm_y <- lm(daly ~ data[, -i])
+            lm_x <- lm(data[, i] ~ data[, -i])
+            out[i, ] <- unlist(cor.test(lm_y$residuals, lm_x$residuals)[4:3], 
+                use.names = FALSE)
+        }
+    }
+    out_list <- list(method = list(method = method, rank = rank, 
+        mapped = mapped), out = out)
+    class(out_list) <- "DALY_sensitivity"
+    return(out_list)
 }
 
 ## PLOT method
@@ -79,7 +60,7 @@ function(x, alpha = 0.05, main = "Sensitivity analysis",
   ## standardized (rank) regression coefficients
   if (x$method$method == "src"){
     ## sort estimates
-    cf <- coef(x$out)[-1, ]
+    cf <- subset(coef(x$out),rownames(coef(x$out)) != "(Intercept)")
     signif <- cf[, 4] < alpha
     order <- order(abs(cf[signif, 1]))
     est <- cf[signif, 1][order]
@@ -129,21 +110,25 @@ function(x, digits = 3, signif_stars = getOption("show.signif.stars"), ...){
   ## standardized (rank) regression coefficients
   if (x$method$method == "src"){
     ## sort estimates
-    cf <- coef(x$out)[-1, ]
+    cf <- subset(coef(x$out),rownames(coef(x$out)) != "(Intercept)")
     order <- order(abs(cf[, 1]), decreasing = TRUE)
     est <- cf[order, 1:4]
+    if (is.vector(est)){
+      est <- as.data.frame(matrix(est, nrow = 1))
+    }
     if (signif_stars){
       signif <-
         symnum(est[, 4], corr = FALSE, na = FALSE, 
                cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1), 
                symbols = c("***", "**", "*", ".", " "))
       est <-
-        data.frame(formatC(est[, 1:2], digits = digits,
+        data.frame(formatC(as.matrix(est[, 1:2]), digits = digits,
                    format = ifelse(x$method$mapped, "f", "g")),
                    formatC(est[, 3], digits = digits, format = "f"),
                    formatC(est[, 4], digits = digits, format = "g"),
                    format(signif))
-      colnames(est) <- c("Estimate", "Std. Error", "t value", "Pr(>|t|)", "")
+      rownames(est) <- rownames(cf)
+      colnames(est) <- c("Estimate", "Std. Error", "t value", "Pr(>|t|)","")
     }
 
     ## define title
